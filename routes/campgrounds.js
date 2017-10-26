e97185ba279fbc7c9132cc91650b233628dfd38d
callmeihor <lifee754@gmail.com>
2017-10-26 23:51:25 +0000

    all

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -2,6 +2,7 @@ var express    = require("express");
 var router     = express.Router();
 var Campground = require("../models/campground");
 var middleware = require("../middleware");
+var geocoder = require("geocoder");
 
 
 // INDEX
@@ -26,7 +27,11 @@ router.post("/", middleware.isLoggedIn, function (req, res) {
         id: req.user._id,
         username: req.user.username
     }
-    var newCampground = {name: name, price: price, image: image, description: desc, author: author}
+    geocoder.geocode(req.body.location, function (err, data) {
+    var lat = data.results[0].geometry.location.lat;
+    var lng = data.results[0].geometry.location.lng;
+    var location = data.results[0].formatted_address;
+    var newCampground = {name: name, image: image, description: desc, cost: cost, author:author, location: location, lat: lat, lng: lng};
     // create a new camp and save to database ept
     Campground.create(newCampground, function (err, newlyCreated) {
         if (err) {
@@ -36,6 +41,7 @@ router.post("/", middleware.isLoggedIn, function (req, res) {
             res.redirect("/slackspots"); //reDIRECT to our page
         }
     });
+});
 });
 
 // NEW - show form to create new campground
@@ -68,16 +74,22 @@ router.get("/:id/edit", middleware.checkCampgroundOwnership, function(req, res) 
 });
 
 // UPDATE CAMPGROUND ROUTE
-router.put("/:id", middleware.checkCampgroundOwnership, function(req, res){
-    //find and update the correct camp
-    Campground.findByIdAndUpdate(req.params.id, req.body.campground, function(err, updatedCampground){
-       if(err) {
-           res.redirect("/slackspots");
-       } else {
-           res.redirect("/slackspots/" + req.params.id);
-       }
+router.put("/:id", function(req, res){
+  geocoder.geocode(req.body.location, function (err, data) {
+    var lat = data.results[0].geometry.location.lat;
+    var lng = data.results[0].geometry.location.lng;
+    var location = data.results[0].formatted_address;
+    var newData = {name: req.body.name, image: req.body.image, description: req.body.description, cost: req.body.cost, location: location, lat: lat, lng: lng};
+    Campground.findByIdAndUpdate(req.params.id, {$set: newData}, function(err, campground){
+        if(err){
+            req.flash("error", err.message);
+            res.redirect("back");
+        } else {
+            req.flash("success","Successfully Updated!");
+            res.redirect("/slackspots/" + campground._id);
+        }
     });
-    //redirect somewhere (show page)
+  });
 });
 // DESTROY CAMPGROUND ROUTE
 router.delete("/:id", middleware.checkCampgroundOwnership, function(req, res){
